8b8556843cc529a498b3e0ffae33e4f8aef063ca
Bhushan Koli <96105166+Bhushankoli28@users.noreply.github.com>
2022-05-19 14:59:27 +0530

    started now

---
+++ mern/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+// import './components/homepage/homepage'
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Bhushan Koli</h1>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ mern/src/components/homepage/homepage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Reducer } from "react";
+
+const homePage = () => ({
+//   <h1> Bhushan Koli</h1>
+<div>
+    <div>Bhushan kolieee</div>
+
+
+</div>
+
+
+});
+
+
+
+
+export default homePage;
